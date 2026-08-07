a4b22f2d8aaf3b7baee1e919593b71cc8d092bf5
shhhaj <2063256723@qq.com>
2026-08-07 23:19:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,103 +1,45 @@
-const fileInput = document.getElementById("fileInput");
-const count = document.getElementById("count");
-const runBtn = document.getElementById("runBtn");
+const input = document.getElementById("photoInput");
 
-let images = [];
+const info = document.getElementById("info");
 
-fileInput.addEventListener("change", async function () {
-    images = [];
+const preview = document.getElementById("preview");
 
-    for (let file of this.files) {
-        const img = new Image();
-        img.src = URL.createObjectURL(file);
 
-        await new Promise(resolve => {
-            img.onload = resolve;
-        });
+let photos = [];
 
-        images.push(img);
-    }
 
-    count.innerHTML = "已导入：" + images.length + " 张照片";
+input.addEventListener("change", function(){
+
+photos = Array.from(this.files);
+
+
+info.innerHTML =
+"已选择 " + photos.length + " 张照片";
+
+
+preview.innerHTML="";
+
+
+photos.forEach(file=>{
+
+
+let img=document.createElement("img");
+
+
+img.src = URL.createObjectURL(file);
+
+
+img.style.width="120px";
+
+img.style.margin="5px";
+
+img.style.borderRadius="10px";
+
+
+preview.appendChild(img);
+
+
 });
 
 
-runBtn.onclick = async function(){
-
-    if(images.length < 2){
-        alert("请至少选择2张照片");
-        return;
-    }
-
-    runBtn.innerHTML="正在银河堆栈，请等待...";
-
-
-    let canvas=document.createElement("canvas");
-    let ctx=canvas.getContext("2d");
-
-
-    canvas.width=images[0].width;
-    canvas.height=images[0].height;
-
-
-    let data=new Float32Array(
-        canvas.width*canvas.height*4
-    );
-
-
-    for(let img of images){
-
-        ctx.drawImage(
-            img,
-            0,
-            0,
-            canvas.width,
-            canvas.height
-        );
-
-
-        let pixels=ctx.getImageData(
-            0,
-            0,
-            canvas.width,
-            canvas.height
-        ).data;
-
-
-        for(let i=0;i<pixels.length;i++){
-            data[i]+=pixels[i];
-        }
-    }
-
-
-    let result=ctx.createImageData(
-        canvas.width,
-        canvas.height
-    );
-
-
-    for(let i=0;i<data.length;i++){
-
-        result.data[i]=
-        Math.min(
-            255,
-            data[i]/images.length*1.15
-        );
-    }
-
-
-    ctx.putImageData(result,0,0);
-
-
-    let link=document.createElement("a");
-    link.download="银河堆栈大师.jpg";
-    link.href=canvas.toDataURL(
-        "image/jpeg",
-        0.95
-    );
-
-    link.click();
-
-
-    runBtn.innerHTML="一键银河完成";
-};
+});
